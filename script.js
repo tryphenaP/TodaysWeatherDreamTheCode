81f8bdd78baea016bff3b94b2f89e443843e283c
Tryphena P <tryphenap@Tryphenas-MacBook-Pro.local>
2026-01-20 10:43:35 -0800

    passed lat and long to open weather site

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 function initAutocomplete() {
-    alert("Options loaded");
+    
     var options= {
         types: ['(cities)'],
 
-    };    
+    };   
+
     var weatherLocationImput = document.getElementById('weather-location');
     var weatherLocationAutocomplete = new google.maps.places.Autocomplete(weatherLocationImput, options);
-    alert(weatherLocationAutocomplete);
-    };
+
+
+    weatherLocationAutocomplete.addListener('place_changed', function() {
+    var weatherLocationLatitude= weatherLocationAutocomplete.getPlace().geometry.location.lat();
+    var weatherLocationLongitude= weatherLocationAutocomplete.getPlace().geometry.location.lng();
+    var weatherAPIURL= `https://api.open-meteo.com/v1/forecast?latitude=${weatherLocationLatitude}&longitude=${weatherLocationLongitude}&hourly=temperature_2m`;
+
+    fetch(weatherAPIURL)
+        .then(response => response.json())
+        .then(data => {
+            var weatherDescription = data.weather[0].description;
+            var temperature = data.main.temp;
+            var humidity = data.main.humidity;
+            var windSpeed = data.wind.speed;
+            console.log(temperature);
+
+            var weatherInfoDiv = document.getElementById('weather-info');
+            weatherInfoDiv.innerHTML = `
+                <h2>Weather Information</h2>
+                <p><strong>Description:</strong> ${weatherDescription}</p>
+                <p><strong>Temperature:</strong> ${temperature} °C</p>
+                <p><strong>Humidity:</strong> ${humidity} %</p>
+                <p><strong>Wind Speed:</strong> ${windSpeed} m/s</p>
+            `;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        }); 
+       });
+
+
+};
